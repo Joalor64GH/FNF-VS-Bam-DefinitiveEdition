23c6e1ad8daaf5e2804299b321aa94dd655d24a3
Joalor64 <rhgarcon@gmail.com>
2022-03-30 10:54:25 -0400

    freeplay menu

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,8 +35,6 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		#if !switch 'donate', #end
 		'options'
