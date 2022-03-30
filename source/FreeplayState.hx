a6c4ec500238558970ad7fe46515a81ebad53880
Joalor64 <rhgarcon@gmail.com>
2022-03-30 10:57:05 -0400

    +1000000000 Social Credit

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -265,7 +265,7 @@ class FreeplayState extends MusicBeatState
 			ratingSplit[1] += '0';
 		}
 
-		scoreText.text = 'PERSONAL BEST: ' + lerpScore + ' (' + ratingSplit.join('.') + '%)';
+		scoreText.text = 'Social Credit: ' + lerpScore + ' (' + ratingSplit.join('.') + '%)';
 		positionHighscore();
 
 		var upP = controls.UI_UP_P;
