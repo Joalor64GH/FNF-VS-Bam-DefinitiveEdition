2ae8e0729e0c652a54c4f3c91e1005533709972c
Joalor64 <rhgarcon@gmail.com>
2022-08-11 09:36:08 -0400

    title watermark

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -371,6 +371,11 @@ class TitleState extends MusicBeatState
 		ngSpr.updateHitbox();
 		ngSpr.screenCenter(X);
 		ngSpr.antialiasing = ClientPrefs.globalAntialiasing;
+		
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "VS Bam Definitive Edition v1.5.0 (PE 0.5.2h)" #if debug + " DEBUG BUILD" #end, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
 
 		FlxTween.tween(credTextShit, {y: credTextShit.y + 20}, 2.9, {ease: FlxEase.quadInOut, type: PINGPONG});
 
